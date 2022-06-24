6f8adc5ffdac9776d5c9741f90bd2f16df45e30b
Alistair Nelson <alistair@alt-nelson.com>
2022-06-24 16:42:49 +0100

    Initial Commit

---
+++ BreathingFlower/BreathingFlowerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BreathingFlowerApp.swift
+//  BreathingFlower
+//
+//  Created by Alistair Nelson on 24/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct BreathingFlowerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
